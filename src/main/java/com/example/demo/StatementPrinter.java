405494bbedfc702d2d22944128bf6f8f560e9ffa
basma <basmamkarrem@gmail.com>
2025-01-29 20:25:30 +0100

    third commit

--- src/main/java/com/example/demo/StatementPrinter.java
+++ src/main/java/com/example/demo/StatementPrinter.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class StatementPrinter {
     public static void print(List<Transaction> transactions) {
         System.out.println("Date       | Amount | Balance");
-        for (int i = transactions.size() - 1; i >= 0; i--) { // Affichage en ordre inverse
+        for (int i = transactions.size() - 1; i >= 0; i--) {
             Transaction t = transactions.get(i);
             System.out.println(t.getDate() + " | " + t.getAmount() + " | " + t.getBalance());
         }
